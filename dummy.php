2ed1a61b56e2adf283475e7fedb664b998cdffc4
vigoagatho <h1101201045@student.untan.ac.id>
2022-08-23 19:04:04 +0700

    new features

--- dummy.php
+++ dummy.php
@@ -2,10 +2,15 @@
 class Aku {
     public $nama;
 
-
+    private $nomorHP;
+    public function ktp(){
+    
+    }
+        
     public function eKtp(){
         
     }
+
 }
 echo "hello world";
 echo "kamu siapa?";
